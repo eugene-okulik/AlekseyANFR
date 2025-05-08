06a4ea3153991ba280c04df20a385aa5d3d7567c
Aleksey ANFR <a.anofriev@yandex.ru>
2025-05-08 16:31:04 +0300

    Homework_7 done update_1

--- homework/aleksey_anofriev/Homework_7/task_3.py
+++ homework/aleksey_anofriev/Homework_7/task_3.py
@@ -3,10 +3,12 @@ b = 'результат операции: 54'
 c = 'результат работы программы: 209'
 d = 'результат: 2'
 
+
 def sum(calc):
     num = int(calc.split()[-1])
     result = num + 10
     print(result)
+
 
 sum(a)
 sum(b)
